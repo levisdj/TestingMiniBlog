224bc006b5e2dece49a30868eff1e813f5edc903
levisdj <levis.dj@gmail.com>
2021-08-28 18:07:28 +0200

    fixing database url param in config file

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess!'
 
     # SQLAlchemy Config
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
